ec34249501a569c59e3d9652328a427a30d299b4
jnfmunoz <juanmunozgrau@gmail.com>
2025-03-22 01:39:26 -0300

    1

---
+++ 02-Variables/02-Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int year = Convert.ToInt16("25");
+double doubleNumber = 2.5;
+string name = "Juan";
+String names = "Juan Francisco";
+decimal decimalNumber = 2.07M;
+float floatNumber = 8.0F;
+
+Console.WriteLine(names + " {0} {1} {2} {3} {4} ", year, doubleNumber, name, decimalNumber, floatNumber);
+
+Console.ReadLine();
+
+// float 7 dígitos 32 bits
+// double 15-16 dígitos 64 bits
+// decimal 28-29 dígitos 128 bits
