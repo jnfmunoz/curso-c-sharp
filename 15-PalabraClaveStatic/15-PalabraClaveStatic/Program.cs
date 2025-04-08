bcdcf6200c28932324c4f0c107e23c7119212b5e
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-07 20:08:40 -0400

    12

---
+++ 15-PalabraClaveStatic/15-PalabraClaveStatic/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+class Program()
+{
+    static void Main()
+    {
+        var data = new Conversor();
+        Conversor.velocidad = 3.0;
+        var data2 = new Conversor();
+    }
+
+    class Conversor
+    {
+        public static double velocidad; // variables estáticas
+
+    }
+}
