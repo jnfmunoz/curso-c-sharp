15fc799e54c5942d1b50296d9aed0a355cbda4c0
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-08 23:55:02 -0400

    13

---
+++ 15.1-PalabraClaveStatic/15.1-PalabraClaveStatic/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Conversor.velocidad = 3.0;
+
+        Conversor.Conversor1();
+        Conversor.Conversor2();
+
+        Console.WriteLine($"Velocidad final: {Conversor.velocidad}");
+        Console.ReadLine();
+    }
+
+    static class Conversor
+    {
+        static Conversor()
+        {
+
+        }
+
+        public static double velocidad; // variables estáticas
+
+        public static void Conversor1()
+        {
+            velocidad += 20;
+        }
+
+        public static void Conversor2()
+        {
+            velocidad += 78;
+        }
+
+    }
+}
