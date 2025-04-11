de8a5ba65f01e8c34139cc494a41ad052f63ba6a
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-10 23:21:12 -0400

    15

--- 16-ArgumentosConNombreYOpcionales/16-ArgumentosConNombreYOpcionales/Program.cs
+++ 16-ArgumentosConNombreYOpcionales/16-ArgumentosConNombreYOpcionales/Program.cs
@@ -1,2 +1,16 @@
 ﻿
 
+class Program
+{
+    static void Main()
+    {
+        var data = new Program();
+        data.Metodo("C#", "Juan"); //, 85);
+
+    }
+
+    private void Metodo(String curso, String nombre, int cantidad = 69) // argumentos opcionales, por defecto
+    {
+        Console.WriteLine(curso + ' ' + nombre + ' ' + cantidad);
+        
+}
