28b70dd1029774f70aec3f7971b5864bb9c2d6b8
jnfmunoz <juanmunozgrau@gmail.com>
2025-06-19 23:58:21 -0400

    46

---
+++ 22-WindowsFormsConNetFramework/Logica/Library/Paginador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Library
+{
+    public class Paginador<T>
+    {
+        private List<T> _dataList;
+        private Label _label;
+        private static int maxReg, _reg_por_pagina, pageCount, numPagi = 1;
+
+        public Paginador(List<T> dataList, Label label, int reg_por_pagina)
+        {
+            _dataList = dataList;
+            _label = label;
+            _reg_por_pagina = reg_por_pagina;
+            cargarDatos();
+
+        }
+
+        private void cargarDatos()
+        {
+            numPagi = 1;
+            maxReg = _dataList.Count;
+            pageCount = (maxReg / _reg_por_pagina);
+
+            // Ajusta el número de página si la última página contiene una parte de la página
+            if ((maxReg % _reg_por_pagina) > 0) 
+            {
+                pageCount += 1;
+            }
+
+            _label.Text = $"Paginas 1/ {pageCount}";
+
+        }
+
+    }
+}
